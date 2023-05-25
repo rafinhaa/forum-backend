557674f156f007741f3a26c2338331157f782a73
Rafinhaa <rafinhaa@me.com>
2023-05-25 19:52:27 -0300

    Fundamentos do DDD - Classe base de entidades

--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,11 +1,8 @@
-import { randomUUID } from "node:crypto";
+import { Entity } from "../../core/entities/entity";
 
-export class Instructor {
-  public id: string;
-  public name: string;
-
-  constructor(name: string, id?: string) {
-    this.name = name;
-    this.id = id ?? randomUUID();
-  }
+interface InstructorProps {
+  id: string;
+  name: string;
 }
+
+export class Instructor extends Entity<InstructorProps> {}
