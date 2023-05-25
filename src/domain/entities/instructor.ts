0ac7635d98393ccca53e1630eec2729a7a5b0c35
Rafinhaa <rafinhaa@me.com>
2023-05-25 20:11:40 -0300

    Fundamentos do DDD - Mapeando propriedades

--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,7 +1,8 @@
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
 interface InstructorProps {
-  id: string;
+  id: UniqueEntityID;
   name: string;
 }
 
