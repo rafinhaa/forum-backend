f67cc45db2850b7cbfe328b7df4ae4c526961353
Rafinhaa <rafinhaa@me.com>
2023-05-25 20:06:31 -0300

    Fundamentos do DDD - ID das entidades

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,15 +1,15 @@
-import { randomUUID } from "node:crypto";
+import { UniqueEntityID } from "./unique-entity-id";
 
 export class Entity<Props> {
-  private _id: string;
-  protected props: any;
+  private _id: UniqueEntityID;
+  protected props: Props;
 
-  get id(): string {
+  get id() {
     return this._id;
   }
 
   constructor(props: Props, id?: string) {
     this.props = props;
-    this._id = id ?? randomUUID();
+    this._id = new UniqueEntityID(id);
   }
 }
