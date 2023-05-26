3f32aff8ebe740b9a44b3ba51e697e44425dff4b
Rafinhaa <rafinhaa@me.com>
2023-05-26 19:36:34 -0300

    Fundamentos do DDD - Getters & Setters das entidades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -3,16 +3,45 @@ import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 import { Optional } from "../../core/types/optional";
 
 interface AnswerProps {
-  content: string;
   authorId: UniqueEntityID;
   questionId: UniqueEntityID;
+  content: string;
   createdAt: Date;
   updatedAt?: Date;
 }
 
 export class Answer extends Entity<AnswerProps> {
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get questionId() {
+    return this.props.questionId;
+  }
+
   get content() {
     return this.props.content;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat("...");
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
   }
 
   static create(
