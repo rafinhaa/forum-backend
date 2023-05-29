3c956d11178d789e9eeb0bd43a15b58dbeb906f2
Rafinhaa <rafinhaa@me.com>
2023-05-29 20:55:33 -0300

    Fórum com DDD & Clean Architecture - Refatorando as pastas

--- src/domain/forum/application/use-cases/__tests__/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/__tests__/answer-question.spec.ts
@@ -1,6 +1,6 @@
 import { AnswerQuestionUseCase } from "../answer-question";
-import { AnswersRepository } from "@/domain/repositories/answers-repository";
-import { Answer } from "@/domain/entities/answer";
+import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository";
+import { Answer } from "@/domain/forum/enterprise/entities/answer";
 
 const fakeAnswersRepository: AnswersRepository = {
   create: async (answer: Answer): Promise<void> => {
