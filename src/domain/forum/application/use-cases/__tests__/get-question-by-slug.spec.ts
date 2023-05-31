1bb9c7156ade97a5a03cdfe85f74d93becde6d80
Rafinhaa <rafinhaa@me.com>
2023-05-31 19:52:49 -0300

    Fórum com DDD & Clean Architecture - Gerando dados fictícios

--- src/domain/forum/application/use-cases/__tests__/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/__tests__/get-question-by-slug.spec.ts
@@ -1,7 +1,6 @@
 import { GetQuestionBySlugUseCase } from "../get-question-by-slug";
 import { InMemoryQuestionsRepository } from "__tests__/repositories/in-memory-questions-repository";
 import { makeQuestion } from "__tests__/factories/make-question";
-import { Slug } from "@/domain/forum/enterprise/entities/values-objects/slug";
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let sut: GetQuestionBySlugUseCase;
@@ -13,18 +12,16 @@ describe("Get Question By Slug", () => {
   });
 
   it("should be able to get a question by slug", async () => {
-    const newQuestion = makeQuestion({
-      slug: Slug.create("example-question-2"),
-    });
+    const newQuestion = makeQuestion();
 
     await inMemoryQuestionsRepository.create(newQuestion);
 
     const { question } = await sut.execute({
-      slug: "example-question-2",
+      slug: newQuestion.slug.value,
     });
 
     expect(question.id).toBeTruthy();
-    expect(question.title).toBe("Example question");
+    expect(question.title).toBe(newQuestion.title);
   });
 
   it("should not be able to get a question by slug", async () => {
