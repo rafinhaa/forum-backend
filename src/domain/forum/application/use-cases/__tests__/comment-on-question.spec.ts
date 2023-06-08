42a4ddb4c664e4832b7609651106c2a006597b70
Rafinhaa <rafinhaa@me.com>
2023-06-08 14:33:25 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Comentar na pergunta

---
+++ src/domain/forum/application/use-cases/__tests__/comment-on-question.spec.ts
@@ -0,0 +1,49 @@
+import { InMemoryQuestionsRepository } from "__tests__/repositories/in-memory-questions-repository";
+import { makeQuestion } from "__tests__/factories/make-question";
+import { makeAnswer } from "__tests__/factories/make-answer";
+import { CommentOnQuestionUseCase } from "../comment-on-question";
+import { InMemoryQuestionCommentsRepository } from "__tests__/repositories/in-memory-question-comments-repository";
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository;
+let sut: CommentOnQuestionUseCase;
+
+describe("Comment on Question", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository();
+
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository
+    );
+  });
+
+  it("should be able to comment on question", async () => {
+    const question = makeQuestion();
+
+    await inMemoryQuestionsRepository.create(question);
+
+    await sut.execute({
+      questionId: question.id.toValue(),
+      authorId: question.authorId.toValue(),
+      content: "content",
+    });
+
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toBe("content");
+  });
+
+  it("should be not find a question", async () => {
+    const question = makeQuestion();
+
+    expect(
+      async () =>
+        await sut.execute({
+          questionId: question.id.toValue(),
+          authorId: question.authorId.toValue(),
+          content: "content",
+        })
+    ).rejects.toBeInstanceOf(Error);
+  });
+});
