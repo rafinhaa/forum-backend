a798840807989da58b96511e0bf6aed0c16c9f8d
Rafinhaa <rafinhaa@me.com>
2023-06-10 10:35:41 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Listar comentários da pergunta

--- __tests__/repositories/in-memory-question-comments-repository.ts
+++ __tests__/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
 import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
 
@@ -9,6 +10,21 @@ export class InMemoryQuestionCommentsRepository
   async findById(id: string): Promise<QuestionComment | null> {
     const item = this.items.find((item) => item.id.toString() === id);
     return item || null;
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams
+  ): Promise<QuestionComment[]> {
+    const questionComments = this.items
+      .filter((comments) => comments.questionId.toString() === questionId)
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice(
+        (params.page - 1) * params.limitPerPage,
+        params.page * params.limitPerPage
+      );
+
+    return questionComments;
   }
 
   async create(questionComment: QuestionComment) {
---
+++ src/domain/forum/application/use-cases/__tests__/fetch-questions-comments.spec.ts
@@ -0,0 +1,93 @@
+import { InMemoryQuestionCommentsRepository } from "__tests__/repositories/in-memory-question-comments-repository";
+import { FetchQuestionsCommentsUseCase } from "../fetch-questions-comments";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { makeQuestionComment } from "__tests__/factories/make-question-comment";
+
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository;
+let sut: FetchQuestionsCommentsUseCase;
+
+const QUESTION1_ID = new UniqueEntityID("question-1");
+
+describe("Fetch Questions Comments", () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository();
+    sut = new FetchQuestionsCommentsUseCase(inMemoryQuestionCommentsRepository);
+  });
+
+  it("should be able to fetch questions comments", async () => {
+    await inMemoryQuestionCommentsRepository.create(
+      makeQuestionComment({
+        createdAt: new Date(2023, 0, 20),
+        questionId: QUESTION1_ID,
+      })
+    );
+    await inMemoryQuestionCommentsRepository.create(
+      makeQuestionComment({
+        createdAt: new Date(2023, 0, 18),
+        questionId: QUESTION1_ID,
+      })
+    );
+    await inMemoryQuestionCommentsRepository.create(
+      makeQuestionComment({
+        createdAt: new Date(2023, 0, 23),
+        questionId: QUESTION1_ID,
+      })
+    );
+
+    const { questionComments } = await sut.execute({
+      questionId: QUESTION1_ID.toString(),
+      page: 1,
+    });
+
+    expect(questionComments).toEqual([
+      expect.objectContaining({
+        createdAt: new Date(2023, 0, 23),
+        questionId: QUESTION1_ID,
+      }),
+      expect.objectContaining({
+        createdAt: new Date(2023, 0, 20),
+        questionId: QUESTION1_ID,
+      }),
+      expect.objectContaining({
+        createdAt: new Date(2023, 0, 18),
+        questionId: QUESTION1_ID,
+      }),
+    ]);
+  });
+
+  it("should be able to fetch paginate que comments", async () => {
+    for (let i = 1; i < 22; i++) {
+      await inMemoryQuestionCommentsRepository.create(
+        makeQuestionComment({
+          createdAt: new Date(2023, 0, i),
+          questionId: QUESTION1_ID,
+        })
+      );
+    }
+    const { questionComments } = await sut.execute({
+      questionId: QUESTION1_ID.toString(),
+      page: 1,
+    });
+
+    expect(questionComments.length).toEqual(20);
+  });
+
+  it("should be able to fetch paginate que comments with limit", async () => {
+    for (let i = 1; i < 10; i++) {
+      await inMemoryQuestionCommentsRepository.create(
+        makeQuestionComment({
+          createdAt: new Date(2023, 0, i),
+          questionId: QUESTION1_ID,
+        })
+      );
+    }
+    const { questionComments } = await sut.execute({
+      questionId: QUESTION1_ID.toString(),
+      page: 1,
+      limitPerPage: 5,
+    });
+
+    expect(questionComments.length).toEqual(5);
+  });
+});
