4d4bf4d336533fafd3f719bcc25cd6754e0c6608
Rafinhaa <rafinhaa@me.com>
2023-06-16 18:44:29 -0300

    Aggregates & Watched Lists - Classe base de AggregateRoot

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,5 +1,5 @@
+import { AggregateRoot } from "@/core/entities/aggregate-root";
 import { Slug } from "./values-objects/slug";
-import { Entity } from "@/core/entities/entity";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Optional } from "@/core/types/optional";
 import dayjs from "dayjs";
@@ -14,7 +14,7 @@ interface QuestionProps {
   updatedAt?: Date;
 }
 
-export class Question extends Entity<QuestionProps> {
+export class Question extends AggregateRoot<QuestionProps> {
   get authorId() {
     return this.props.authorId;
   }
