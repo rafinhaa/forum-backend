8b7c8984a61dc27d318729edcc3f0c99f310addd
Rafinhaa <rafinhaa@me.com>
2023-08-29 18:38:24 -0300

    Subdomínios & Domain Events - Estrutura de eventos de domínio

--- src/core/entities/unique-entity-id.ts
+++ src/core/entities/unique-entity-id.ts
@@ -14,4 +14,8 @@ export class UniqueEntityID {
   constructor(value?: string) {
     this.value = value ?? randomUUID();
   }
+
+  public equals(id: UniqueEntityID): boolean {
+    return id.toValue() === this.value;
+  }
 }
