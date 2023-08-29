36226c4baa6a6fabdc8a94de68657adfbb27dea3
Rafinhaa <rafinhaa@me.com>
2023-08-29 19:00:18 -0300

    Subdomínios & Domain Events - Evento: resposta criada

---
+++ src/domain/forum/events/answer-created-event.ts
@@ -0,0 +1,16 @@
+import { DomainEvent } from "@/core/events/domain-event";
+import { Answer } from "../enterprise/entities/answer";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+export class AnswerCreatedEvent implements DomainEvent {
+  public ocurredAt: Date;
+  private answer: Answer;
+
+  constructor(answer: Answer) {
+    this.answer = answer;
+    this.ocurredAt = new Date();
+  }
+  getAggregateId(): UniqueEntityID {
+    return this.answer.id;
+  }
+}
